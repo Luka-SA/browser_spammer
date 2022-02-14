b21d1f6aff62ff106b7c5124aa6ae67e03df7ee6
Luka-SA <87711438+Luka-SA@users.noreply.github.com>
2022-02-14 00:21:29 -0500

    Update browser_spammer.py

--- browser_spammer.py
+++ browser_spammer.py
@@ -5,7 +5,7 @@
 Author:  Luka Senfner
 ID:      210729560
 Email:   senfl5620@gmail.com
-__updated__ = "2022-02-12"
+__updated__ = "2022-02-14"
 -------------------------------------------------------
 """
 # Imports
@@ -16,14 +16,13 @@ def browser():
     URL = str(input("Paste the URL of the website you want to go to: "))
     num_times_open = int(
         input("How many times do you want the link to open in a new tab?: "))
-    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
     if num_times_open > 0:
-        webbrowser.get(chrome_path).open(URL, 1)
+        webbrowser.get('windows-default').open(URL, 1)
     else:
         print("You broke me!  Try again")
     if num_times_open > 1:
         for i in range(num_times_open - 1):
-            webbrowser.get(chrome_path).open(URL)
+            webbrowser.get('windows-default').open_new_tab(URL)
 
 
 browser()
